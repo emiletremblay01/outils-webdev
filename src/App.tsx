de346f0942064c3f4bbca346b2e624f9b0ef91fa
Emile Tremblay <emiletremblay01@gmail.com>
2023-05-16 12:22:14 -0400

    added more sites

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { Grid } from "./components/Grid";
-import { MenuDropdown } from "./components/MenuDropdown";
+
 import { GridItemProps } from "./types/types";
 
 const unstlyedComponentsSites: GridItemProps[] = [
@@ -25,12 +25,33 @@ const animationSites: GridItemProps[] = [
   { name: "framer motion", link: "https://www.framer.com/motion/" },
   { name: "animate.css", link: "https://animate.style/" },
 ];
+const colorSites: GridItemProps[] = [
+  { name: "Color Hunt", link: "https://colorhunt.co/" },
+  {
+    name: "Realtime Colors",
+    link: "https://realtimecolors.com/?colors=000000-ffffff-4685ff-f2f2f2-ffb084",
+  },
+  { name: "ColorSpace", link: "https://mycolor.space/" },
+];
+const iconsSites: GridItemProps[] = [
+  { name: "Heroicons", link: "https://heroicons.com/" },
+  { name: "Remix Icon", link: "https://remixicon.com/" },
+  { name: "fontawesome", link: "https://fontawesome.com/" },
+];
+const miscSites: GridItemProps[] = [
+  { name: "fontjoy", link: "https://fontjoy.com/" },
+  { name: "haikei", link: "https://app.haikei.app/" },
+  { name: "JSON Crack", link: "https://jsoncrack.com/" },
+];
 export default function App() {
   return (
-    <div className="flex h-screen gap-2 p-4 mx-auto text-white bg-black lg:container ">
+    <div className="flex flex-wrap h-screen gap-2 p-4 mx-auto text-white bg-black lg:container ">
       <Grid name="unstyled components" items={unstlyedComponentsSites} />
       <Grid name="backend testing" items={backendTestingSites} />
       <Grid name="animation" items={animationSites} />
+      <Grid name="color" items={colorSites} />
+      <Grid name="icons" items={iconsSites} />
+      <Grid name="misc" items={miscSites} />
     </div>
   );
 }
